27ad68a29adbf91a540478559697fca31eeff858
brad@smithwebtek.com <brad@smithwebtek.com>
2021-05-27 07:38:56 -0400

    Update deployment configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock "3.7.2"
+lock "3.14.1"
 
 set :application, "mattapan-mapping-api"
 set :repo_url, "git@github.com:smithWEBtek/mattapan-mapping-api.git"
